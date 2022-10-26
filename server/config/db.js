242872609c46a7a554c7db05fcad7cb360539fab
AmanKaurPreet <amanpreetkaur11152003@gmail.com>
2022-10-26 19:52:40 -0400

    first

---
+++ server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //local MongoDB deployment ->
+  "URI": "mongodb+srv://Amanpreet:Amanpreet@assignmentmongodb.sqs6fph.mongodb.net/?retryWrites=true&w=majority",
+  "Secret":"SomeSecret"
+};
